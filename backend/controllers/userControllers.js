024f1637c55677d04c6dd06c50fd1aa089fa1367
amalraj-g <g.amalraj1993@gmail.com>
2024-01-29 19:19:46 +0530

    add new updated

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -40,21 +40,16 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('User already exists');
     }
 
-    const user = await User.create({
-        name,email,password
-    });
-
-    if (user) {
-
-        generateToken(res, user._id)
-
+    const user = await User.create({name,email,password});
+    if (user !== userExists) {
         res.status(201).json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
         })
-    } else {
+    } 
+    else {
         res.status(400);
         throw new Error('Invalid user data')
     }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,10 +11,11 @@ import {
     updateUser,
   } from '../controllers/userControllers.js';
 import { protect, admin } from '../middleware/authMiddleware.js'
+import { registerValidation } from '../middleware/validationMiddleware.js'
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.route('/').post(registerValidation, registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/auth', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
